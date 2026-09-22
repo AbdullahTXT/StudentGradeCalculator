86d2ea797cd8d94254ec5734b20075a807838d1b
AbdullahTXT <bdallhshlghwm5@gmail.com>
2026-09-22 17:32:10 +0200

    Add basic calculator program

---
+++ calculator.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int number1 = 20;
+    int number2 = 5;
+
+    cout << "Sum: "
+         << number1 + number2 << endl;
+
+    return 0;
+}
