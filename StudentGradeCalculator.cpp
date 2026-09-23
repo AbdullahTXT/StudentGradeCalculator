116b8bcaa9208ed29c64e795bea56eeffae6f6cc
AbdullahTXT <bdallhshlghwm5@gmail.com>
2026-09-23 09:55:10 +0200

    Nationnality Feature Added

--- StudentGradeCalculator.cpp
+++ StudentGradeCalculator.cpp
@@ -30,6 +30,7 @@ int main()
          << numberOfStudents << endl;
 
     // Feature: Calculate the average of grades for a student added
+    // Feature: Nationality of the student added
 
     return 0;
 }
